bfa9b4c197997cde987adf631eb9deaf743a9ea0
TechAurelian <dev@techaurelian.com>
2024-10-18 16:51:57 +0300

    Build the Flutter Web app with GitHub Actions

--- lib/utils/color_utils.dart
+++ lib/utils/color_utils.dart
@@ -1,3 +1,8 @@
+// Copyright (c) 2024 Tecdrop. All rights reserved.
+// Use of this source code is governed by an MIT-style license that can be
+// found in the LICENSE file or at
+// https://github.com/tecdrop/rgb_easy_mix/blob/main/LICENSE.
+
 import 'package:flutter/material.dart';
 
 /// Returns the black or white contrast color of a given color.
